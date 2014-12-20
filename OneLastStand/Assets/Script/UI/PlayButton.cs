6177403119e37a8c415446143438f20c3f55a1f5
mathiasmusset <mussetmathias@gmail.com>
2014-12-20 01:09:14 +0100

    Menu Start (non fonctionnel)

---
+++ OneLastStand/Assets/Script/UI/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+
+	public GameMain _gameMain;
+
+	// Use this for initialization
+	void Onclick () {
+
+		//Déplacer caméra puis
+		_gameMain.enabled = true;
+	}
+}
